8aad4fd5749f7b2c6fde20b4eaac2997e761e1b9
Kurtis Jackson Smith <kurtisjsmith@gmail.com>
2026-07-08 18:32:34 +0900

    Update script.js

---
+++ js/script.js
@@ -0,0 +1,21 @@
+const command = "man maggie";
+
+let i = 0;
+
+const target = document.getElementById("command");
+
+function type(){
+
+    if(i < command.length){
+
+        target.textContent += command.charAt(i);
+
+        i++;
+
+        setTimeout(type,120);
+
+    }
+
+}
+
+type();
