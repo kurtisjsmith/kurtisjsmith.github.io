b1b9b9a8dd936c1d2f69f86b7d1f591df933e7d3
Margaret Smith <bimbobana@gmail.com>
2026-07-09 19:22:58 +0900

    Added dymanic time and date to login status

--- js/typing.js
+++ js/typing.js
@@ -1,4 +1,48 @@
+function getLastLogin() {
+
+    const now = new Date();
+
+    const days = [
+        "Sun",
+        "Mon",
+        "Tue",
+        "Wed",
+        "Thu",
+        "Fri",
+        "Sat"
+    ];
+
+    const months = [
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec"
+    ];
+
+    const day = days[now.getDay()];
+    const month = months[now.getMonth()];
+
+    const date = String(now.getDate()).padStart(2, " ");
+
+    const hour = String(now.getHours()).padStart(2, "0");
+    const minute = String(now.getMinutes()).padStart(2, "0");
+    const second = String(now.getSeconds()).padStart(2, "0");
+
+    return `Last login: ${day} ${month} ${date} ${hour}:${minute}:${second} on ttys000`;
+
+}
+
+
 const startupCommand = "kurtisjsmith";
+
 
 function typeCommand(onComplete) {
 
@@ -6,7 +50,7 @@ function typeCommand(onComplete) {
     const prompt = document.createElement("div");
 
     prompt.innerHTML =
-        "Last login: Wed Jul 8 14:36:18 on ttys000<br><br>" +
+        `${getLastLogin()}<br><br>` +
         "kurtisjsmith@portfolio ~ % <span id='command'></span><span class='cursor'>█</span>";
 
     output.appendChild(prompt);
@@ -14,6 +58,7 @@ function typeCommand(onComplete) {
     const commandSpan = prompt.querySelector("#command");
 
     let index = 0;
+
 
     function type() {
 
